a5b1cf325e4ed7e3eab45054f6765024bd659412
ymatuhin <ymatuhin@yandex.ru>
2016-07-28 22:00:29 +0300

    Add yandex and other stuff

--- 1.index.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([1],{43:function(e,t){"use strict";Object.defineProperty(t,"__esModule",{value:!0});var s={test:1};t["default"]=s}});
---
+++ home.js
@@ -0,0 +1,11 @@
+'use strict'
+
+import '../_components/registration/reg.js'
+
+const debug = window.debug('📄 home')
+
+export default class Home {
+	constructor() {
+		debug('init')
+	}
+}
