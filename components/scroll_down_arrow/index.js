40c7f22bcfda55eb0e8064bc48522d9b4db4a406
Yury Matuhin <ym@ymatuhin.ru>
2017-02-22 21:58:23 +0300

    Build index page styles

--- components/scroll_down_arrow/index.js
+++ components/scroll_down_arrow/index.js
@@ -4,35 +4,27 @@ const logger = window.debug('⬇️ scoll down')
 
 import './index.styl'
 import html from './index.html'
-import { getPercentScroll } from 'services/scroll_helpers'
 
 class Arrow {
 	constructor(el) {
 		this.$el = document.querySelector(el)
 		this.mount()
-		this.listeners()
 	}
 
 	mount() {
 		logger('mount')
 		this.$el.innerHTML = html
 		this.$svg = this.$el.querySelector('svg')
+		this.listeners()
 	}
 
 	listeners() {
 		logger('listeners')
-
-		this.onScroll()
 		window.addEventListener('scroll', this.onScroll.bind(this), false)
 	}
 
 	onScroll() {
-		if (this.isInvisible()) return
-		this.$el.style.opacity = 1 - getPercentScroll()
-	}
-
-	isInvisible() {
-		return window.getComputedStyle(this.$svg, null).getPropertyValue('display') === 'none'
+		this.$el.style.opacity = 0
 	}
 }
 
